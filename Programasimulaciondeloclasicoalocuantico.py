7ce9baa790c2cd1c1a081bf5cba17af3fa121e44
andrewcubillos <32993313+andrewcubillos@users.noreply.github.com>
2019-09-23 19:31:36 -0500

    Update Programasimulaciondeloclasicoalocuantico.py

--- Programasimulaciondeloclasicoalocuantico.py
+++ Programasimulaciondeloclasicoalocuantico.py
@@ -1,17 +1,73 @@
+from numeroscomplejos import *
+def norm(N):
+    return ((((N[0])**2)+((N[1])**2))**(1/2))**2
 
-def state(X,Y,n)
-    X =[[1 for x in range(6)] for y in range(6)]
-    Y=([[1],[2],[3],[4],[5],[6]])
+
+
+def state(X,Y,n):
+                
     result =[[0 for x in range(len(Y[0]))] for y in range(len(Y))]
-    for h in range(n)
+    for h in range(n-1):
         for i in range(len(X)):
            
            for j in range(len(Y[0])):
                
                for k in range(len(Y)):
-                   result[i][j] += X[i][k] * Y[k][j]
+            
                    
+                   result[i][j] += X[i][k] * Y[k][j]
+            
+        Y=result
+     
+    return result 
 
-print (result)
+def stateprobabilistic(X,Y,n):
+    result =[[0 for x in range(len(Y[0]))] for y in range(len(Y))]
+    p=X
+    result2=[[0 for x in range(len(p[0]))] for y in range(len(p))]
+    
+    for h in range(n-1):
+        for i in range(len(X)):
+           
+           for j in range(len(p[0])):
+               
+               for k in range(len(p)):
+                   result2[i][j] += X[i][k] * p[k][j]
+                   
+                        
+
+    for i in range(len(result2)):
+       
+       for j in range(len(Y[0])):
+           
+           for k in range(len(Y)):
+               result[i][j] += result2[i][k] * Y[k][j]
+         
+    return result
 
 
+
+def quantumstate(X,n):
+    Y=X
+    result = [[0 for j in range(len(Y[0]))] for i in range(len(Y))]
+    ssum=(0,0)
+    for h in range(n):
+        for i in range(len(X)):
+            
+           for j in range(len(Y[0])):
+
+               for k in range(len(Y)):
+                   result[i][j] =suma(ssum,producto(X[i][k],Y[k][j]))
+                   
+                   ssum=result[i][j]       
+               ssum =(0,0)
+
+    for z in result:
+        for m in z:
+            if m==(0,0):
+                result[result.index(z)][z.index(m)]=0
+            elif m==(1,0):
+                result[result.index(z)][z.index(m)]=1
+            else:
+                result[result.index(z)][z.index(m)]=norm(m)
+    return result       
